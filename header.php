180e5d8f684dbfc38c0e955ec7afcf8ad3ab5a92
Dario Devcic <dariodevcic@gmail.com>
2016-06-29 16:24:40 +0200

    Code cleanup.

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 
 <head <?php hybrid_attr( 'head' ); ?>>
 <?php wp_head(); // Hook required for scripts, styles, and other <head> items. ?>
-
 </head>
 
 <body <?php hybrid_attr( 'body' ); ?>>
@@ -15,25 +14,24 @@
 		</div><!-- .skip-link -->
 
 		<header <?php hybrid_attr( 'header' ); ?>>
-            
-            <div <?php hybrid_attr( 'branding' ); ?>> 
-                <div id="access" class="site-access grid-container">
-                    <div class="grid-100 relative">
-               
-						<?php //themelia_build_logo(); ?>
-                        <?php themelia_construct_site_title(); ?>
+			
+			<div <?php hybrid_attr( 'branding' ); ?>> 
+				<div id="access" class="site-access grid-container">
+					<div class="grid-100 relative">
 
-                        <?php hybrid_get_menu( 'primary' ); // Loads the menu/primary.php template. ?>
-                    
-                    </div><!-- .grid-100 -->
-                </div><!-- #access -->
-            </div><!-- #branding -->    
+						<?php themelia_construct_site_title(); ?>
+
+						<?php hybrid_get_menu( 'primary' ); // Loads the menu/primary.php template. ?>
+					
+					</div><!-- .grid-100 -->
+				</div><!-- #access -->
+			</div><!-- #branding -->    
 
 		</header><!-- #header -->
 
 		<div id="main" class="main">
 			<div class="grid-container">
-            
+			
 			<?php if ( true == get_theme_mod( 'display_breadcrumbs', true ) ) : ?>
-                <?php hybrid_get_menu( 'breadcrumbs' ); // Loads the menu/breadcrumbs.php template. ?>
-            <?php endif; ?>
+				<?php hybrid_get_menu( 'breadcrumbs' ); // Loads the menu/breadcrumbs.php template. ?>
+			<?php endif; ?>
