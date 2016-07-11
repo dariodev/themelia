d4c06313403c0a5672ac660e6b97b3ae6e75f298
Dario Devcic <dariodevcic@gmail.com>
2016-07-11 03:19:40 +0200

    Escaping home_url

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label>
         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'themelia' ) ?></span>
         <input type="search" class="search-field"
