bc69c55c7b2b0ea81019d58c2e9dc0b743a434aa
Dario Devcic <dariodevcic@gmail.com>
2016-10-10 01:45:27 +0200

    Code Formatting

--- content/content.php
+++ content/content.php
@@ -1,9 +1,9 @@
 <article <?php hybrid_attr( 'post' ); ?>>
-	
+
 	<?php
 		$css_class = ' zero-comments';
 		$number    = (int) get_comments_number( get_the_ID() );
-		
+
 		if ( 1 === $number )
 			$css_class = ' one-comment';
 		elseif ( 1 < $number )
@@ -22,7 +22,7 @@
 				<?php comments_popup_link( ( '' ), '<span>' . number_format_i18n( 1 ) . '</span>' . __( ' Comment', 'themelia' ), '<span>%</span>' .  __( ' Comments', 'themelia' ), 'comments-link' . $css_class, '' ); ?>
 				<?php edit_post_link(); ?>
 			</div><!-- .entry-byline -->
-			
+
 			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
 
 			<?php themelia_post_thumbnail(); ?>
@@ -38,7 +38,7 @@
 			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'sep' => '<span>,</span> ',  'text' => esc_html__( 'Posted in: %s', 'themelia' ) ) ); ?>
 			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'sep' => '<span>,</span> ', 'text' => esc_html__( 'Tagged: %s', 'themelia' ), 'before' => '<br />' ) ); ?>
 		</footer><!-- .entry-footer -->
-		
+
 		<?php get_template_part( 'misc/author-box' ); ?>
 
 	<?php else : // If not viewing a single post. ?>
@@ -55,7 +55,7 @@
 			</div><!-- .entry-byline -->
 
 			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . esc_url( get_permalink() ) . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
-			
+
 			<?php themelia_post_thumbnail(); ?>
 
 		</header><!-- .entry-header -->
