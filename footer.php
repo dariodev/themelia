f75a74be59c520dc2dc2c9f5f1ebd3de93f9f14f
Dario Devcic <dariodevcic@gmail.com>
2017-01-12 16:43:40 +0100

    Reordering elements
    menu 1st, credits 2nd

--- footer.php
+++ footer.php
@@ -7,17 +7,17 @@
 		<footer <?php hybrid_attr( 'footer' ); ?>>
 
 			<div class="grid-container site-footer-inner">
-				<div class="grid-50 tablet-grid-50">
+				<div class="grid-50 tablet-grid-50 push-50 tablet-push-50">
+					<?php hybrid_get_menu( 'subsidiary' ); // Loads the menu/subsidiary.php template. ?>
+			 	</div><!-- .grid-50 -->
+				<div class="grid-50 tablet-grid-50 pull-50 tablet-pull-50">
                     <p class="credit">
-                        <?php printf(
-                            // Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link.
+                    	<?php printf(
+                        	// Translators: 1 is current year, 2 is site name/link, 3 is WordPress name/link, and 4 is theme name/link.
                             esc_html__( 'Copyright &#169; %1$s %2$s. Powered by %3$s and %4$s.', 'themelia' ),
                             date_i18n( 'Y' ), hybrid_get_site_link(), hybrid_get_wp_link(), hybrid_get_theme_link()
                         ); ?>
                     </p><!-- .credit -->
-			 	</div><!-- .grid-50 -->
-				<div class="grid-50 tablet-grid-50">
-					<?php hybrid_get_menu( 'subsidiary' ); // Loads the menu/subsidiary.php template. ?>
 			 	</div><!-- .grid-50 -->
 			</div><!-- .grid-container -->
 
